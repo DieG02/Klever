0d6495c177b10ed0da341e6a146a95dabd41e4de
DieG02 <diegob0505@gmail.com>
2024-02-22 16:14:43 +0000

    Seeting up main structure

---
+++ src/styles/global.ts
@@ -0,0 +1,13 @@
+export const Colors = {
+  primary: '#4378FE',
+  alternative: '#F7F9FF',
+  text: '#666666',
+  link: '#4378FE',
+  placeholder: '#999999',
+  background: '#FAFAFA',
+  white: '#FFFFFF',
+  light: '#F7F9FF',
+  gray: '#CCCCCC',
+  dark: '#444444',
+  black: '#252525',
+}
