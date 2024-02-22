f2312a5e36c14a977400759050bdf1ac3f6ee0e1
DieG02 <diegob0505@gmail.com>
2024-02-22 20:25:29 +0000

    Adding Poppins to the project

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,13 +1,20 @@
-export const Colors = {
-  primary: '#4378FE',
-  alternative: '#F7F9FF',
-  text: '#666666',
-  link: '#4378FE',
-  placeholder: '#999999',
-  background: '#FAFAFA',
-  white: '#FFFFFF',
-  light: '#F7F9FF',
-  gray: '#CCCCCC',
-  dark: '#444444',
-  black: '#252525',
-}
+export enum Colors {
+  Primary = '#4378FE',
+  Alternative = '#F7F9FF',
+  Text = '#666666',
+  Link = '#4378FE',
+  Placeholder = '#999999',
+  Background = '#FAFAFA',
+  White = '#FFFFFF',
+  Light = '#F7F9FF',
+  Gray = '#CCCCCC',
+  Dark = '#444444',
+  Black = '#252525',
+};
+
+export enum Poppins {
+  Regular = 'Poppins-Regular',
+  Medium = 'Poppins-Medium',
+  Semibold = 'Poppins-SemiBold',
+  Bold = 'Poppins-Bold',
+};
