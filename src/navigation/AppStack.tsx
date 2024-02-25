e5c8ec448742381c9599f8b3ea2cd1423992f559
DieG02 <diegob0505@gmail.com>
2024-02-25 18:50:15 +0000

    Adding heroicons for manage icons in the app

--- src/navigation/AppStack.tsx
+++ src/navigation/AppStack.tsx
@@ -1,15 +1,39 @@
 import { createStackNavigator } from '@react-navigation/stack';
-import { AppStackParamList } from '../types/Navigation';
-import Home from '../screens/Home';
+import { ChevronLeftIcon as ChevronLeftIconMini } from 'react-native-heroicons/mini';
+
 import Collection from '../screens/Collection';
+import Home from '../screens/Home';
+import { Colors, Poppins } from '../styles/global';
+import { AppStackParamList } from '../types/Navigation';
 
 const AppStack = createStackNavigator<AppStackParamList>();
 
 export default function AppNavigator() {
   return (
     <AppStack.Navigator>
-      <AppStack.Screen name='Home' component={Home} options={{ headerShown: false }}/>
-      <AppStack.Screen name='Collection' component={Collection} />
+      <AppStack.Screen 
+        name='Home'
+        component={Home}
+        options={{ headerShown: false }}/>
+      <AppStack.Screen 
+        name='Collection'
+        component={Collection}
+        options={({ route }) => ({ 
+          title: route.params.title,
+          headerStyle: {
+            backgroundColor: Colors.White,
+            elevation: 0
+          },
+          headerTintColor: Colors.Dark,
+          headerTitleStyle: {
+            fontFamily: Poppins.Semibold,
+            fontSize: 16,
+          },
+          headerTitleAlign: 'center',
+          headerBackImage: () => (
+            <ChevronLeftIconMini color={ Colors.Dark }/>
+          )
+        })}/>
     </AppStack.Navigator>
   )
 };
