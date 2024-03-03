f65ee089292c4dbfaadca0cda8ed79be4ef4bbea
DieG02 <diegob0505@gmail.com>
2024-03-03 20:05:04 +0000

    Rename package name to Klever

--- ios/KleverTests/KleverTests.m
+++ ios/KleverTests/KleverTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface KlverTests : XCTestCase
+@interface KleverTests : XCTestCase
 
 @end
 
-@implementation KlverTests
+@implementation KleverTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
