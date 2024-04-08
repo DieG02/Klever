efca66aa5bd8cbcd0c1adbe927d4636d7d781b03
DieG02 <diegob0505@gmail.com>
2024-04-08 19:55:44 +0100

    Adding PasswordField

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,7 +1,8 @@
 import Heading from './Heading';
 import InputField from './InputField';
 import MainButton from './MainButton';
+import PasswordField from './PasswordField';
 import Separator from './Separator';
 import Spacing from './Spacing';
 
-export { Heading, InputField, MainButton, Separator, Spacing };
+export { Heading, InputField, MainButton, PasswordField, Separator, Spacing };
